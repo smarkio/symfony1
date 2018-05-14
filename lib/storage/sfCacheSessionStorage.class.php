7387a4a5aad2f3e2d6ab4e6394357e6df40c0557
Rui Campos <rui.campos@smark.io>
2018-05-14 11:06:21 +0100

    NO-ISSUE: Option to not send cache control headers for session

--- lib/storage/sfCacheSessionStorage.class.php
+++ lib/storage/sfCacheSessionStorage.class.php
@@ -32,6 +32,7 @@ class sfCacheSessionStorage extends sfStorage
    *                        session_cookie_lifetime [required] liftime of cookie
    *                        session_cookie_secure [required] send only if secure connection
    *                        session_cookie_http_only [required] accessible only via http protocol
+   *                        session_send_cache_control_header [optional] set it to false to not send the cache control headers
    *
    * @return bool true, when initialization completes successfully.
    *
@@ -40,21 +41,22 @@ class sfCacheSessionStorage extends sfStorage
   public function initialize($options = array())
   {
     // initialize parent
-    
+
     // bc with a slightly different name formerly used here, let's be
     // compatible with the base class name for it from here on out
     if (isset($options['session_cookie_http_only']))
     {
       $options['session_cookie_httponly'] = $options['session_cookie_http_only'];
     }
-    
+
     parent::initialize(array_merge(array('session_name' => 'sfproject',
                                          'session_cookie_lifetime' => '+30 days',
                                          'session_cookie_path' => '/',
                                          'session_cookie_domain' => null,
                                          'session_cookie_secure' => false,
                                          'session_cookie_httponly' => true,
-                                         'session_cookie_secret' => 'sf$ecret'), $options));
+                                         'session_cookie_secret' => 'sf$ecret',
+                                         'session_send_cache_control_header', true), $options));
 
     // create cache instance
     if (isset($this->options['cache']) && $this->options['cache']['class'])
@@ -145,7 +147,10 @@ class sfCacheSessionStorage extends sfStorage
       }
     }
     session_id($this->id);
-    $this->response->addCacheControlHttpHeader('private');
+    if( !isset($this->options['session_send_cache_control_header']) || $this->options['session_send_cache_control_header'] )
+    {
+        $this->response->addCacheControlHttpHeader('private');
+    }
 
     return true;
   }
